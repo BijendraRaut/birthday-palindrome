f084cfd0db97cea2e5da11b16b7db332728b9654
Bijendra Raut <bijendraghvm@gmail.com>
2023-05-11 21:15:41 +0530

    getAllDateFormats fn

--- script.js
+++ script.js
@@ -26,3 +26,16 @@ function convertDateToStr(date) {
   dateStr.year = date.year.toString();
   return dateStr;
 }
+
+function getAllDateFormats(date) {
+  var dateStr = convertDateToStr(date);
+
+  var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+  var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+  var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+  var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+  var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+  var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+
+  return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+}
