f9964860b4aa77ec8a2435dd5635f4daffc9e556
Bijendra Raut <bijendraghvm@gmail.com>
2023-05-11 21:00:50 +0530

    datetostr fn

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ function isPalindrome(str) {
   const reversed = reverseStr(str);
   return reversed === str;
 }
+
+function convertDateToStr(date) {
+  var dateStr = { day: "", month: "", year: "" };
+
+  if (date.day < 10) {
+    dateStr.day = "0" + date.day;
+  } else {
+    dateStr.day = date.day.toString();
+  }
+
+  if (date.month < 10) {
+    dateStr.month = "0" + date.month;
+  } else {
+    dateStr.month = date.month.toString();
+  }
+
+  dateStr.year = date.year.toString();
+  return dateStr;
+}
